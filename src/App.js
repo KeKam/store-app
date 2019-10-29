f433610dc7e3daef674e4f350e93ce22f686bbe8
Kenta Kamimura <kenta.stolt.kamimura@gmail.com>
2019-10-29 13:20:05 +0100

    Add createGlobalStyle

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import { Switch, Route } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage';
 import ShopPage from './pages/shop-page/shop-page';
+import { GlobalStyle } from './components/global-styles/global-style';
 
 const App = () => {
   return (
     <div>
+      <GlobalStyle />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route exact path='/shop' component={ShopPage} />
