5b4be9a0171fdc71d1a2e7c7f9c359bf0755c0c2
Kenta Kamimura <kenta.stolt.kamimura@gmail.com>
2020-02-03 14:09:15 +0100

    Add lazy and suspense

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, lazy, Suspense } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 
-import HomePage from './pages/homepage/homepage';
-import CollectionPage from './pages/collection-page/collection-page';
-import CheckoutPage from './pages/checkout-page/checkout-page';
-import LoginPage from './pages/login-page/login-page';
 import Header from './components/header/header';
+import Spinner from './components/spinner/spinner';
 import { selectCurrentUser } from './redux/user/user.selectors';
 import { checkUserSession } from './redux/user/user.actions';
 import { GlobalStyle } from './components/global-styles/global-style';
+
+const HomePage = lazy(() => import('./pages/homepage/homepage'));
+
+const CollectionPage = lazy(() =>
+  import('./pages/collection-page/collection-page')
+);
+
+const CheckoutPage = lazy(() => import('./pages/checkout-page/checkout-page'));
+
+const LoginPage = lazy(() => import('./pages/login-page/login-page'));
 
 const App = () => {
   const currentUser = useSelector(selectCurrentUser);
@@ -24,14 +31,16 @@ const App = () => {
       <GlobalStyle />
       <Header />
       <Switch>
-        <Route exact path='/' component={HomePage} />
-        <Route path='/collection' component={CollectionPage} />
-        <Route exact path='/checkout' component={CheckoutPage} />
-        <Route
-          exact
-          path='/login'
-          render={() => (currentUser ? <Redirect to='/' /> : <LoginPage />)}
-        />
+        <Suspense fallback={<Spinner />}>
+          <Route exact path='/' component={HomePage} />
+          <Route path='/collection' component={CollectionPage} />
+          <Route exact path='/checkout' component={CheckoutPage} />
+          <Route
+            exact
+            path='/login'
+            render={() => (currentUser ? <Redirect to='/' /> : <LoginPage />)}
+          />
+        </Suspense>
       </Switch>
     </div>
   );
