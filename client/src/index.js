f35d03355f0b80e6a56159effbef61e17115aaaf
Kenta Kamimura <kenta.stolt.kamimura@gmail.com>
2020-02-18 19:59:22 +0100

    Add code for PWA functionality

--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,7 @@ import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 
 import { store, persistor } from './redux/store/store.config';
+import * as serviceWorker from './serviceWorker';
 import App from './App';
 
 ReactDOM.render(
@@ -17,3 +18,5 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 );
+
+serviceWorker.register();
